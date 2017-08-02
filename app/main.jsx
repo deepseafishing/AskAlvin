2495a5a17f48d1104ffac3f4fb17a09a6e913af9
deepseafishing <onejunekang@gmail.com>
2017-08-02 18:03:07 -0400

    fix: replace with new bone

--- app/main.jsx
+++ app/main.jsx
@@ -8,14 +8,9 @@
 import 'babel-polyfill'
 
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider, connect } from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom'
+import {render} from 'react-dom'
+import {Provider, connect} from 'react-redux'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 
 import store from './store'
 import Jokes from './components/Jokes'
@@ -23,20 +18,22 @@ import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
 
-const ExampleApp = connect(({ auth }) => ({
-  user: auth
-}))(({ user, children }) =>
-  <div>
-    <nav>
-      {user ? <WhoAmI /> : <Login />}
-    </nav>
-    <main>
-      <Switch>
-        <Route path="/jokes" component={Jokes} />
-        <Route component={NotFound} />
-      </Switch>
-    </main>
-  </div>
+const ExampleApp = connect(
+  ({ auth }) => ({ user: auth })
+)(
+  ({ user, children }) =>
+    <div>
+      <nav>
+        {user ? <WhoAmI/> : <Login/>}
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/jokes" component={Jokes} />
+          <Redirect exact from="/" to="/jokes" />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </div>
 )
 
 render(

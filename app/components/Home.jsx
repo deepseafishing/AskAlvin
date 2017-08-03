133d7629a90ac113fc2323f61fc51bce638f26ce
deepseafishing <onejunekang@gmail.com>
2017-08-02 21:25:08 -0400

    initial signup login

---
+++ app/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import axios from 'axios';
+import { Link } from 'react-router-dom'
+import { MediaBox, Row, Col } from 'react-materialize'
+
+const Home = props => {
+  return (
+    <div>
+      <MediaBox src="/images/alvintheman.jpg" alt="askalvin" width="1050" />
+    </div>
+  )
+}
+export default Home

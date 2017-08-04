e3acf7463f810cc5e593bfc6888f8a7f418450db
stanleytiu7 <stanleytiu7@gmail.com>
2017-08-04 17:56:28 -0400

    toggle infobox on marker click, added info to infobox, added currently logged in user info through react-redux connect, added multi marker functionality

--- app/components/SimpleMapExample.jsx
+++ app/components/SimpleMapExample.jsx
@@ -1,7 +1,15 @@
 /* global google */
-import { default as React, Component } from 'react'
-
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import {
+  default as React,
+  Component
+} from 'react'
+import {connect} from 'react-redux'
+import {
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from 'react-google-maps'
 
 import SearchBox from 'APP/node_modules/react-google-maps/lib/places/SearchBox'
 
@@ -37,7 +45,13 @@ const SearchBoxExampleGoogleMap = withGoogleMap(props =>
       inputStyle={INPUT_STYLE}
     />
     {props.markers.map((marker, index) =>
-      <Marker position={marker.position} key={index} />
+      <Marker position={marker.position} key={index} onClick={() => props.onMarkerClicker(marker) } >
+        {marker.showInfo && (
+          <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+            <div>{marker.infoContent}</div>
+          </InfoWindow>
+        )}
+      </Marker>
     )}
   </GoogleMap>
 )
@@ -47,67 +61,153 @@ const SearchBoxExampleGoogleMap = withGoogleMap(props =>
  *
  * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
  */
-export default class SearchBoxExample extends Component {
-  state = {
-    bounds: null,
-    center: {
-      lat: 47.6205588,
-      lng: -122.3212725
-    },
-    markers: []
-  }
+class SearchBoxExample extends Component {
+ state = {
+   bounds: null,
+   center: {
+     lat: 40.704593,
+     lng: -74.009577
+   },
+   markers: []
+ }
 
-  handleMapMounted = this.handleMapMounted.bind(this)
-  handleBoundsChanged = this.handleBoundsChanged.bind(this)
-  handleSearchBoxMounted = this.handleSearchBoxMounted.bind(this)
-  handlePlacesChanged = this.handlePlacesChanged.bind(this)
+ handleMapMounted = this.handleMapMounted.bind(this)
+ handleBoundsChanged = this.handleBoundsChanged.bind(this)
+ handleSearchBoxMounted = this.handleSearchBoxMounted.bind(this)
+ handlePlacesChanged = this.handlePlacesChanged.bind(this)
+ handleMarkerClicker = this.handleMarkerClicker.bind(this)
+ handleMarkerClose = this.handleMarkerClose.bind(this)
 
-  handleMapMounted(map) {
-    this._map = map
-  }
+ handleMapMounted(map) {
+   this._map = map
+ }
 
-  handleBoundsChanged() {
-    this.setState({
-      bounds: this._map.getBounds(),
-      center: this._map.getCenter()
-    })
-  }
+ handleBoundsChanged() {
+   this.setState({
+     bounds: this._map.getBounds(),
+     center: this._map.getCenter()
+   })
+ }
 
-  handleSearchBoxMounted(searchBox) {
-    this._searchBox = searchBox
-  }
+ handleSearchBoxMounted(searchBox) {
+   this._searchBox = searchBox
+ }
 
-  handlePlacesChanged() {
-    const places = this._searchBox.getPlaces()
-    console.log(places)
-    // Add a marker for each place returned from search bar
-    const markers = places.map(place => ({
-      position: place.geometry.location
-    }))
+ handlePlacesChanged() {
+   const places = this._searchBox.getPlaces()
+   console.log('user:', this.props.currentUser)
+   console.log(places)
+   // Add a marker for each place returned from search bar
+   const markers = places.map(place => ({
+     position: place.geometry.location,
+     infoContent:
+    ([
+      <div key={1}>Name: {places[0].name} </div>,
+      <div key={2}>Address: {places[0].formatted_address}</div>,
+      <div key={3}>Phone: {places[0].formatted_phone_number||places[0].international_phone_number}</div>,
+      <div key={4}>Website: {places[0].website}</div>
 
-    // Set markers; set map center to first search result
-    const mapCenter =
-      markers.length > 0 ? markers[0].position : this.state.center
+    ]),
+     showInfo: false
+   }))
+   console.log('array', markers)
 
-    this.setState({
-      center: mapCenter,
-      markers
-    })
-  }
+   const mapCenter =
+   markers.length > 0 ? markers[0].position : this.state.center
 
-  render() {
-    return (
-      <SearchBoxExampleGoogleMap
-        containerElement={<div style={{ height: `100vh` }} />}
-        mapElement={<div style={{ height: `100vh` }} />}
-        center={this.state.center}
-        onMapMounted={this.handleMapMounted}
-        onBoundsChanged={this.handleBoundsChanged}
-        onSearchBoxMounted={this.handleSearchBoxMounted}
-        bounds={this.state.bounds}
-        onPlacesChanged={this.handlePlacesChanged}
-        markers={this.state.markers}
-      />
-    )
-  }
+   this.setState({
+     center: mapCenter,
+     markers: [...this.state.markers, ...markers]
+   })
+   // axios.
+ }
+
+ handleMarkerClicker(targetMarker) {
+   console.log('here')
+   console.log('marker: ', targetMarker)
+   if (targetMarker.showInfo) {
+     this.setState({
+       markers: this.state.markers.map(marker => {
+         if (marker === targetMarker) {
+           return {
+             ...marker,
+             showInfo: false,
+           }
+         }
+         return marker
+       }),
+     })
+   } else {
+     this.setState({
+       markers: this.state.markers.map(marker => {
+         if (marker === targetMarker) {
+           return {
+             ...marker,
+             showInfo: true,
+           }
+         }
+         return marker
+       }),
+     })
+   }
+ }
+
+ handleMarkerClose(targetMarker) {
+   this.setState({
+     markers: this.state.markers.map(marker => {
+       if (marker !== targetMarker) return marker
+       // if (marker === targetMarker) {
+       // return {
+       // ...marker,
+       // showInfo: false,
+       // }
+       // }
+       // return marker
+     }),
+   })
+ }
+
+ render() {
+   return (
+     <SearchBoxExampleGoogleMap
+       containerElement={<div style={{ height: `100vh` }} />
+       }
+       mapElement = {
+         <div style={{ height: `100vh` }} />
+       }
+       center = {
+         this.state.center
+       }
+       onMapMounted = {
+         this.handleMapMounted
+       }
+       onBoundsChanged = {
+         this.handleBoundsChanged
+       }
+       onSearchBoxMounted = {
+         this.handleSearchBoxMounted
+       }
+       bounds = {
+         this.state.bounds
+       }
+       onPlacesChanged = {
+         this.handlePlacesChanged
+       }
+       markers = {
+         this.state.markers
+       }
+       onMarkerClicker = {
+         this.handleMarkerClicker
+       }
+       onMarkerClose = {
+         this.handleMarkerClose
+       }
+     />
+   )
+ }
 }
+const mapStateToProps = state => ({
+  currentUser: state.auth
+})
+
+export default connect(mapStateToProps, null)(SearchBoxExample)
